b9ca27831aee80ca32ae6b81a73f37ade6f57a85
Alejandro Candelas Fernández <alejandrocf89@gmail.com>
2019-03-15 11:51:26 +0100

    TEST 1 - MAKE A FORM
    El test 1 ha sido realizado.
    
    Se ha creado el componente test1. El formulario realizado para introducir ordenadores consta de 8 campos. Se ha decido que los campos "type" y "shape" fueran de tipo "select" ya que únicamente podían tomar valores fijos. La ruta a la imagen se forma a partir de una ruta fija "resources/images/jsonPcImages/", y los otros dos directorios lo forman el campo "shape" y el campo "brand".

---
+++ js/controllers/viewControllers/test1Controller.js
@@ -0,0 +1,58 @@
+/*  "use strict" is to indicate that the code should be executed in "strict mode" 
+*	see https://www.w3schools.com/js/js_strict.asp for more details.
+*/
+'use strict';
+
+/**
+ * @autor Alejandro Candelas
+ * @mail alejandrocf89@gmail.com
+ * @date 14/03/2019
+ * @description Test view controller.
+ */
+app.controller('test1Controller', ['$rootScope', '$scope',
+	function ($rootScope, $scope) {
+		$scope.submitted = false;
+		$scope.image = "";
+
+		/**
+		 * @autor Alejandro Candelas
+		 * @mail alejandrocf89@gmail.com
+		 * @date 14/03/2019
+		 * @description Submittion function. Checks if form is valid.
+		 */
+		$scope.update = function (computer) {
+			if ($scope.computerForm.$valid) {
+				console.log('COMPUTER', computer);
+				$scope.submitted = true;
+				computer.urlImagePath = 'resources/images/jsonPcImages/' + computer.shape + '/' + computer.brand;
+				$rootScope.computers.push(computer);
+			} else {
+				alert('Required fields must be filled in.');
+			}
+		};
+	}
+])
+/**
+ * @autor Alejandro Candelas
+ * @mail alejandrocf89@gmail.com
+ * @date 14/03/2019
+ * @description 'fileread' directive obtains file data.
+ */
+.directive("fileread", [function () {
+	return {
+		$scope: {
+			fileread: "="
+		},
+		link: function ($scope, element, attributes) {
+			element.bind("change", function (changeEvent) {
+				var reader = new FileReader();
+				reader.onload = function (loadEvent) {
+					$scope.$apply(function () {
+						$scope.image = loadEvent.target.result;
+					});
+				}
+				reader.readAsDataURL(changeEvent.target.files[0]);
+			});
+		}
+	}
+}]);
