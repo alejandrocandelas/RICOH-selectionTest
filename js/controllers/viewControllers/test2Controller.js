26bf36921764a54bd0a1251e31931a495a1c241b
Alejandro Candelas Fernández <alejandrocf89@gmail.com>
2019-03-16 16:19:11 +0100

    TEST 2.B - CAROUSEL
    Se ha realizado un carrusel para mostrar los ordenadores portátiles y sus características. Se han introducido las funcionalidades de avanzar y retroceder, ir a cualquier registro haciendo click en los puntos que describen qué registro está actuvo, y que vaya pasando de registro automáticamente.

--- js/controllers/viewControllers/test2Controller.js
+++ js/controllers/viewControllers/test2Controller.js
@@ -11,6 +11,7 @@
  */
 app.controller('test2Controller', ['$rootScope', '$scope',
 	function ($rootScope, $scope) {
+		// TEST 1.A
 		$scope.desktopComputers = $rootScope.computers.filter(item => item.shape == 'desktop');
 		$scope.dtConfig = $rootScope.dtConfig;
 
@@ -30,7 +31,7 @@ app.controller('test2Controller', ['$rootScope', '$scope',
 			}
 			return pages;
 		}
-		
+
 		/**
 		 * @autor Alejandro Candelas
 		 * @mail alejandrocf89@gmail.com
@@ -54,5 +55,80 @@ app.controller('test2Controller', ['$rootScope', '$scope',
 					$scope.dtConfig.first = first;
 			}
 		}
+
+		// TEST 1.B
+		$scope.laptopComputers = $rootScope.computers.filter(item => item.shape == 'laptop');
+		$scope.interval = undefined;
+
+		$scope.slideIndex = 1;
+
+		/**
+		 * @autor Alejandro Candelas
+		 * @mail alejandrocf89@gmail.com
+		 * @date 16/03/2019
+		 * @description Sets the active banner and dot.
+		 */
+		$scope.showSlides = function (n) {
+			let i;
+			let slides = document.getElementsByClassName("mySlides");
+			let dots = document.getElementsByClassName("dot");
+			console.log(slides, dots);
+			if (n > slides.length) { $scope.slideIndex = 1 }
+			if (n < 1) { $scope.slideIndex = slides.length }
+			for (i = 0; i < slides.length; i++) {
+				slides[i].style.display = "none";
+			}
+			for (i = 0; i < dots.length; i++) {
+				dots[i].className = dots[i].className.replace(" active", "");
+			}
+			slides[$scope.slideIndex - 1].style.display = "block";
+			dots[$scope.slideIndex - 1].className += " active";
+		}
+
+		/**
+		 * @autor Alejandro Candelas
+		 * @mail alejandrocf89@gmail.com
+		 * @date 16/03/2019
+		 * @description Waits until view is load to run showSlides function.
+		 */
+		angular.element(function () {
+			$scope.showSlides($scope.slideIndex);
+		});
+
+		// Next/previous controls
+		$scope.plusSlides = function (n) {
+			console.log(n);
+			$scope.showSlides($scope.slideIndex += n);
+		}
+
+		// Thumbnail image controls
+		$scope.currentSlide = function (n) {
+			$scope.showSlides($scope.slideIndex = n);
+		}
+
+		/**
+		 * @autor Alejandro Candelas
+		 * @mail alejandrocf89@gmail.com
+		 * @date 16/03/2019
+		 * @description Starts autoplay.
+		 */
+		$scope.start = function () {
+			$scope.interval = setTimeout(function () {
+				$scope.plusSlides(1);
+				$scope.start();
+			}, 3000); // Change image every 3 seconds
+		}
+
+		/**
+		 * @autor Alejandro Candelas
+		 * @mail alejandrocf89@gmail.com
+		 * @date 16/03/2019
+		 * @description Stops autoplay.
+		 */
+		$scope.stop = function () {
+			clearInterval($scope.interval);
+			$scope.interval = undefined;
+		}
+
 	}
 ]);
